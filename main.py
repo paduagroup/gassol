3e7691bf861a4a465ec367e4056b997cdb0b0546
Zheng Gong <unimisia@gmail.com>
2021-02-01 13:39:18 +0100

    Add USB devices detector. UI update.

--- main.py
+++ main.py
@@ -4,29 +4,52 @@ import sys
 import argparse
 from PyQt5 import QtWidgets
 from qtgassol.ui import MainUI
-from qtgassol.device import Thermometer, Manometer, DummyT, DummyP, DummyFile
+from qtgassol.device import Thermometer, Manometer, DummyT, DummyP, DummyFile, DeviceDetector
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--temp', type=str, default='dummy', help='Device for thermometer')
-parser.add_argument('-p', '--press', type=str, default='dummy', help='Device for manometer')
-parser.add_argument('-o', '--output', type=str, default='output.txt', help='Output filename')
-parser.add_argument('--dt', type=float, default=1.0, help='Time interval for reading data')
+parser.add_argument('-t', '--temp', type=str, default='auto', help='Device for thermometer.'
+                                                                   'auto means detect the thermometer automatically.'
+                                                                   'dummy means use randomly generated temperature data.'
+                                                                   'Otherwise specify the device e.g. /dev/ttyUSB0, or a file name')
+parser.add_argument('-p', '--press', type=str, default='auto', help='Device for manometer.'
+                                                                    'auto means detect the manometer automatically.'
+                                                                    'dummy means use randomly generated pressure data.'
+                                                                    'Otherwise specify the device e.g. /dev/ttyUSB0, or a file name')
+parser.add_argument('-o', '--output', type=str, default='output.txt', help='Output filename.'
+                                                                           'Can also be specified from GUI.')
+parser.add_argument('--dt', type=float, default=3.0, help='Time interval for reading data.'
+                                                          'Can also be specified from GUI.')
 
 opt = parser.parse_args()
 
 if __name__ == '__main__':
-    if opt.temp == 'dummy':
+    if opt.temp == 'auto':
+        temp = DeviceDetector.detect_thermometer()
+        if temp is None:
+            print('ERROR: Thermometer not detected. Try again or specify the device.')
+            sys.exit(1)
+        else:
+            print('Thermometer detected: %s' % temp)
+    elif opt.temp == 'dummy':
         temp = DummyT()
-    else:
+    elif opt.temp.startswith('/dev'):
         temp = Thermometer(opt.temp)
-
-    if opt.press == 'dummy':
-        press = DummyP()
     else:
-        press = Manometer(opt.press)
+        temp = DummyFile(opt.temp, -2)
 
-    # temp = DummyFile('./test/Ar_filling_111220.out', -2)
-    # press = DummyFile('./test/Ar_filling_111220.out', -1)
+    if opt.press == 'auto':
+        press = DeviceDetector.detect_manometer()
+        if press is None:
+            print('ERROR: Manometer not detected. Try again or specify the device.')
+            sys.exit(1)
+        else:
+            print('Manometer detected: %s' % press)
+    elif opt.press == 'dummy':
+        press = DummyP()
+    elif opt.press.startswith('/dev'):
+        press = Manometer(opt.press)
+    else:
+        press = DummyFile(opt.press, -1)
 
     app = QtWidgets.QApplication(sys.argv)
     ui = MainUI(temp, press, opt.output, opt.dt)
--- qtgassol/device.py
+++ qtgassol/device.py
@@ -1,4 +1,5 @@
 import serial
+import serial.tools.list_ports
 import numpy as np
 from datetime import datetime
 
@@ -14,7 +15,7 @@ class Device(object):
             self.port = serial.Serial(port, 9600, timeout=1.0)
 
     def __str__(self):
-        return (self.name + ': ' + str(self.port))
+        return ('%s : %s' % (self.name, self.port))
 
     def read(self):
         '''
@@ -29,23 +30,24 @@ class Thermometer(Device):
     def __init__(self, port):
         super().__init__('Thermometer', port)
 
-        self.port.write(b'T\r')
-
     def read(self):
         self.port.write(b'T\r')
         self.port.reset_input_buffer()
         buf = self.port.read(16)
         print(buf)
 
-        datastr = buf.decode()
-        if len(datastr):
-            try:
-                val = float(datastr.split(':')[1].strip().split()[0])
-                return val
-            except:
-                pass
+        # make sure the format is correct
+        # b'T\r\nt:   32.728 C'
+        words = buf.decode().split()
+        if len(words) < 2 or words[-1] != 'C':
+            return -1
 
-        return -1
+        try:
+            val = float(words[-2])
+        except:
+            return -1
+        else:
+            return val
 
 
 class Manometer(Device):
@@ -53,11 +55,11 @@ class Manometer(Device):
 
     def __init__(self, port):
         super().__init__('Manometer', port)
-        self.port = serial.Serial(port, 9600, timeout=1.0)
 
-        self.port.write(b'xQ,2\r')  # speed 2: 16000 cycles 1.0 s
-        #       3:  8000 cycles 0.5 s
-        self.port.write(b'xU,0\r')  # units: mbar
+        # speed 2: 16000 cycles 1.0 s
+        # speed 3:  8000 cycles 0.5 s
+        self.port.write(b'xQ,2\r')
+        self.port.write(b'xU,0\r')  # unit: mbar
         self.port.write(b'x*A,1.0\r')  # send value every 1.0 s
 
     def read(self):
@@ -66,15 +68,18 @@ class Manometer(Device):
         buf = self.port.read(13)
         print(buf)
 
-        datastr = buf.decode()
-        if len(datastr):
-            try:
-                val = float(datastr.split()[0])
-                return val
-            except:
-                pass
+        # make sure the format is correct
+        # b'962.43 mbar\r\n'
+        words = buf.decode().split()
+        if len(words) < 2 or words[-1] != 'mbar':
+            return -1
 
-        return -1
+        try:
+            val = float(words[-2])
+        except:
+            return -1
+        else:
+            return val
 
 
 class DummyT(Device):
@@ -121,3 +126,39 @@ class DummyFile(Device):
 
             words = line.strip().split()
             return float(words[self.column])
+
+
+class DeviceDetector():
+    '''
+    Detect USB device for thermometer and manometer.
+    '''
+
+    @staticmethod
+    def detect_thermometer():
+        devices = [p.device for p in serial.tools.list_ports.comports()]
+        for d in devices:
+            if not d.startswith('/dev/ttyUSB'):
+                continue
+
+            dev = serial.Serial(d, 9600, timeout=1.0)
+            dev.write(b'T\r')
+            buf = dev.read(16)
+            if buf.decode().strip().endswith('C'):
+                return Thermometer(d)
+
+        return None
+
+    @staticmethod
+    def detect_manometer():
+        devices = [p.device for p in serial.tools.list_ports.comports()]
+        for d in devices:
+            if not d.startswith('/dev/ttyUSB'):
+                continue
+
+            dev = serial.Serial(d, 9600, timeout=1.0)
+            dev.write(b'x*R\r')
+            buf = dev.read(13)
+            if buf.decode().strip().endswith('mbar'):
+                return Manometer(d)
+
+        return None
